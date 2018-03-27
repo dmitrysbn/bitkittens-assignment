18bd2f4a668bf82a90587c4cc778ca7168289a57
dmitrysbn <dmitry.sbn@gmail.com>
2018-03-27 16:34:42 -0400

    It's a rave ting

--- cats.js
+++ cats.js
@@ -1,43 +1,45 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   var summonKittiesButton = document.querySelector('.summon-cats');
-  var listOfVisitors = document.querySelector('#list-of-cats')
+  var listOfVisitors = document.querySelector('#list-of-cats');
+  var main = document.querySelector('main');
 
   summonKittiesButton.addEventListener('click', function() {
-    $.ajax({
-      url: 'http://bitkittens.herokuapp.com/cats.json',
-      method: 'GET',
-      data: {number: 5},
-      dataType: 'json'
 
-    }).fail(function() {
-      console.log("FAILED");
+    setInterval(function() {
 
-    }).done(function(responseData) {
-      var cats = responseData.cats;
+      $.ajax({
+        url: 'http://bitkittens.herokuapp.com/cats.json',
+        method: 'GET',
+        data: {number: 5},
+        dataType: 'json'
 
-      cats.forEach(function(cat, index) {
-        var img = document.createElement('img');
-        img.src = cat.photo;
-        img.alt = "Photo of " + cat.name;
+      }).fail(function() {
+        console.log("FAILED");
 
-        document.querySelector('#cat' + (index + 1)).innerHTML = img.outerHTML;
+      }).done(function(responseData) {
+        var cats = responseData.cats;
 
-        if (cat.name === "") {
-          cat.name = "Jane Doe";
-        } else if (cat.name === "Ray&Egon") {
-          cat.name = "Ray and Egon";
-        }
+        cats.forEach(function(cat, index) {
+          var img = document.createElement('img');
+          img.src = cat.photo;
+          img.alt = "Photo of " + cat.name;
 
-        console.log(cat.name);
+          document.querySelector('#cat' + (index + 1)).innerHTML = img.outerHTML;
 
-        if (!listOfVisitors.querySelector('[data-name="' + cat.name + '"]')) { // if cat hasn't yet visited
+          if (cat.name === "") {
+            cat.name = "Jane Doe";
+          } else if (cat.name === "Ray&Egon") {
+            cat.name = "Ray and Egon";
+          }
+
+          if (!listOfVisitors.querySelector('[data-name="' + cat.name + '"]')) { // if cat hasn't yet visited
           var catListItem = document.createElement('li');
 
           catListItem.dataset.name = cat.name;
           catListItem.dataset.count = 1;
 
-          catListItem.innerText = cat.name + " x1";
+          catListItem.innerText = cat.name;
           listOfVisitors.append(catListItem);
 
         } else if (listOfVisitors.innerHTML.includes(cat.name)) { // if cat has visited
@@ -48,16 +50,20 @@ document.addEventListener('DOMContentLoaded', function() {
           var i = catItem.dataset.count;
 
           catItem.innerText = cat.name + " x" + i;
-
-
-
         }
+
+        });
       });
-    });
+
+    }, 100);
+
+    setInterval(function() {
+      main.style.background = 'url("/home/dmitry/Desktop/bitmaker/assignments/Week 10/tuesday-03-bitkittens-assignment/images/yardinverted.png") no-repeat';
+
+      setTimeout(function() {
+        main.style.background = 'url("/home/dmitry/Desktop/bitmaker/assignments/Week 10/tuesday-03-bitkittens-assignment/images/yard.png") no-repeat';
+      }, 50);
+      
+    }, 100);
   });
-
-
-
-
-
-})
+});
