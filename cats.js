42e4a2542e0686c0c8bdbbed7dd0339d566738ae
dmitrysbn <dmitry.sbn@gmail.com>
2018-03-27 16:06:21 -0400

    Add unique cat counter

--- cats.js
+++ cats.js
@@ -1,21 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   var summonKittiesButton = document.querySelector('.summon-cats');
+  var listOfVisitors = document.querySelector('#list-of-cats')
 
   summonKittiesButton.addEventListener('click', function() {
     $.ajax({
       url: 'http://bitkittens.herokuapp.com/cats.json',
       method: 'GET',
-      data: {},
-      dataType: 'json',
-      // crossDomain: true
-      // headers: 'Access-Control-Allow-Origin:*'
+      data: {number: 5},
+      dataType: 'json'
 
     }).fail(function() {
       console.log("FAILED");
-      // var img = document.createElement('img');
-      // console.log(img);
-
 
     }).done(function(responseData) {
       var cats = responseData.cats;
@@ -27,8 +23,37 @@ document.addEventListener('DOMContentLoaded', function() {
 
         document.querySelector('#cat' + (index + 1)).innerHTML = img.outerHTML;
 
-      })
-    })
+        if (cat.name === "") {
+          cat.name = "Jane Doe";
+        } else if (cat.name === "Ray&Egon") {
+          cat.name = "Ray and Egon";
+        }
+
+        console.log(cat.name);
+
+        if (!listOfVisitors.querySelector('[data-name="' + cat.name + '"]')) { // if cat hasn't yet visited
+          var catListItem = document.createElement('li');
+
+          catListItem.dataset.name = cat.name;
+          catListItem.dataset.count = 1;
+
+          catListItem.innerText = cat.name + " x1";
+          listOfVisitors.append(catListItem);
+
+        } else if (listOfVisitors.innerHTML.includes(cat.name)) { // if cat has visited
+          var catItem = listOfVisitors.querySelector('[data-name="' + cat.name + '"]'); // [data-name="name"]
+
+          catItem.dataset.count++; // increase number of times the cat visited by 1
+
+          var i = catItem.dataset.count;
+
+          catItem.innerText = cat.name + " x" + i;
+
+
+
+        }
+      });
+    });
   });
 
 
